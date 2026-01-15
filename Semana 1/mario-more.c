0cb354880d5e5ac99bae835b9e6976d7f3bf5ac7
wund-coder <wundantunes@gmail.com>
2026-01-15 18:25:19 -0300

    Add files via upload

---
+++ Semana 1/mario-more.c
@@ -0,0 +1,41 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	do
+	{
+      printf("Altura: ");
+      scanf("%d", &n);
+	}
+	 while (n<1 || n>8);
+	 
+        for (int i = 0; i < n; i++)
+    {
+		for (int e = 0; e < n - 1 - i; e++)
+	 {
+		    printf(" ");
+	 }
+	  printf("  ")
+		for (int j = 0; j <= i; j++)
+        {
+    printf("#");
+        }
+    printf("\n");
+    }
+    printf("  ");
+
+
+      
+		    return 0;
+}
+		
+	
